80992861aec0458547d7a73f1aa15f77c7707bb2
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2024-01-08 12:29:49 -0800

    print matrix integer

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=None):
+    if matrix is None:
+        return
+
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end=" ")
+        print()
