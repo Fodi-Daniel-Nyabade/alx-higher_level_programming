d01c3728a51b8beae0fa19b0c40701ee15c1ff47
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2024-01-30 15:18:48 -0800

    lock class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 class LockedClass:
+    """
+    This class defines a LockedClass that allows only the 'first_name' attribute to be set.
+
+    Attributes:
+        first_name (str): The first name of the instance.
+    """
     __slots__ = ('first_name',)
